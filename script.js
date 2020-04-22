91f4e9a856542210fc285f4fb5dff8107820d5c6
unknown <carlos.delgado.dev@gmail.com>
2020-04-22 14:00:28 -0500

    js constructors

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-var gameCharacter = {
-    name: "Nimish",
-    class: "Human",
-    health: 100,
-    get title() {
-        return this.name + " the " + this.class;
-    },
-    set maxHealth(h) {
-        this.health = h;
-    },
-    xPos: 0,
-    items: ["Knife", "Food"],
-    move: function(x) {
+// var gameCharacter = {
+//     name: "Nimish",
+//     class: "Human",
+//     health: 100,
+//     get title() {
+//         return this.name + " the " + this.class;
+//     },
+//     set maxHealth(h) {
+//         this.health = h;
+//     },
+//     xPos: 0,
+//     items: ["Knife", "Food"],
+//     move: function(x) {
+//         this.xPos += x;
+//     }
+// };
+
+
+
+
+function gameCharacter (name, xPos, health) {
+    this.name = name;
+    this.xPos = xPos;
+    this.health = health;
+    this.move = function (x) {
         this.xPos += x;
     }
-};
+    this.class = "Human";
+}
+
+var gc1 = new gameCharacter("Nimish", 0, 100);
+var gc2 = new gameCharacter("Zenva", 5, 150);
 
