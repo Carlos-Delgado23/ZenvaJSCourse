0879cc3b690540b3b6ecea57f2e785fa41276f18
unknown <carlos.delgado.dev@gmail.com>
2020-04-22 13:50:28 -0500

    getters and setter js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var gameCharacter = {
     name: "Nimish",
+    class: "Human",
+    health: 100,
+    get title() {
+        return this.name + " the " + this.class;
+    },
+    set maxHealth(h) {
+        this.health = h;
+    },
     xPos: 0,
     items: ["Knife", "Food"],
     move: function(x) {
         this.xPos += x;
     }
 };
+
