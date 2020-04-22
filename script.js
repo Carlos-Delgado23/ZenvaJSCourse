0238584c5d313cef590a67cbab7842645b05e4fd
unknown <carlos.delgado.dev@gmail.com>
2020-04-22 15:45:31 -0500

    class constructor for js

--- script.js
+++ script.js
@@ -17,26 +17,53 @@
 
 
 
+// PROTOTYPING
+// function gameCharacter (name, xPos, health) {
+//     this.name = name;
+//     this.xPos = xPos;
+//     this.health = health;
+//     this.move = function (x) {
+//         this.xPos += x;
+//     }
+// }
 
-function gameCharacter (name, xPos, health) {
-    this.name = name;
-    this.xPos = xPos;
-    this.health = health;
-    this.move = function (x) {
+// var gc1 = new gameCharacter("Nimish", 0, 100);
+
+// var gc2 = new gameCharacter("Zenva", 5, 150);
+
+// gameCharacter.prototype.this.class = "Human";
+
+
+// var heal = function(amount) {
+//     this.health += amount;
+// }
+
+// gameCharacter.prototype.heal = heal;
+
+
+
+
+
+// CLASS CONSTRUCTOR
+class GameCharacter {
+
+    constructor (name, xPos, health) {
+        this.name = name;
+        this.xPos = xPos;
+        this.health = health;
+    }
+
+    move (x) {
         this.xPos += x;
     }
 }
 
-var gc1 = new gameCharacter("Nimish", 0, 100);
-
-var gc2 = new gameCharacter("Zenva", 5, 150);
-
-gameCharacter.prototype.this.class = "Human";
-
-
-var heal = function(amount) {
-    this.health += amount;
+class HumanCharacter extends GameCharacter {
+    constructor (name, xPos, health) {
+        super (name, xPos, health);
+        this.classification = "Human";
+    }
 }
 
-gameCharacter.prototype.heal = heal;
-
+var gc1 = new gameCharacter("Nimish", 0, 100);
+var hc1 = new HumanCharacter("Zenva", 5, 150);
